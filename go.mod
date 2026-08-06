9d0f2216fe8be3b5de51dfa45f181630d1c10b2c
Johnny <johnnyflores@live.ca>
2026-08-06 16:32:57 -0400

    Add addStudent and vieStudent func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module student-management-system
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import "fmt"
+
+type Student struct {
+	ID   int
+	Name string
+	Age int
+	Grade string
+}
+
+var students []Student
+
+func main() {
+
+	for {
+
+		fmt.Println("\n===== Student Management System =====")
+		addStudent()
+		viewStudents()
+		fmt.Println("3. Search Student")
+		fmt.Println("4. Update Student")
+		fmt.Println("5. Delete Student")
+		fmt.Println("6. Exit")
+
+		var choice int
+
+		fmt.Print("Choose an option: ")
+		fmt.Scan(&choice)
+
+		switch choice {
+
+		case 1:
+			fmt.Println("Add Student selected")
+
+		case 2:
+			fmt.Println("View Student selected")
+
+		case 3:
+			fmt.Println("Search Student selected")
+
+		case 4:
+			fmt.Println("Update Student selected")
+
+		case 5:
+			fmt.Println("Delete Student selected")
+
+		case 6:
+			fmt.Println("Goodbye!")
+			return
+
+		default:
+			fmt.Println("Invalid option")
+		}
+	}
+}
+
+func addStudent() {
+
+	var student Student
+
+	fmt.Print("Enter ID: ")
+	fmt.Scan(&student.ID)
+
+	fmt.Print("Enter Name: ")
+	fmt.Scan(&student.Name)
+
+	fmt.Print("Enter Age: ")
+	fmt.Scan(&student.Age)
+
+	fmt.Print("Enter Grade: ")
+	fmt.Scan(&student.Grade)
+
+	students = append(students, student)
+
+	fmt.Println("Student added successfully!")
+}
+
+func viewStudents() {
+
+	if len(students) == 0 {
+		fmt.Println("No students found")
+		return
+	}
+
+	for _, student := range students {
+
+		fmt.Println("----------------")
+		fmt.Println("ID:", student.ID)
+		fmt.Println("Name:", student.Name)
+		fmt.Println("Age:", student.Age)
+		fmt.Println("Grade:", student.Grade)
+	}
+}
