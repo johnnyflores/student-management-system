9e135be386b763f043db2c17d046aa27f62971b6
Johnny <johnnyflores@live.ca>
2026-08-06 22:33:28 -0400

    Implemented updated and delete student

--- api/routes.go
+++ api/routes.go
@@ -4,19 +4,47 @@ import (
 	"net/http"
 )
 
-
 func RegisterRoutes(mux *http.ServeMux, handler *StudentHandler) {
 
 	mux.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
+
 		switch r.Method {
+
 		case http.MethodGet:
 			handler.GetStudents(w, r)
+
 		case http.MethodPost:
 			handler.CreateStudent(w, r)
+
 		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			http.Error(
+				w,
+				"Method not allowed",
+				http.StatusMethodNotAllowed,
+				)
 		}
 	})
 
-	mux.HandleFunc("/student", handler.GetStudent)
+
+	mux.HandleFunc("/student", func(w http.ResponseWriter, r *http.Request) {
+
+		switch r.Method {
+
+		case http.MethodGet:
+			handler.GetStudent(w, r)
+
+		case http.MethodPut:
+			handler.UpdateStudent(w, r)
+
+		case http.MethodDelete:
+			handler.DeleteStudent(w, r)
+
+		default:
+			http.Error(
+				w,
+				"Method not allowed",
+				http.StatusMethodNotAllowed,
+			)
+		}
+	})
 }
--- api/student_handler.go
+++ api/student_handler.go
@@ -43,6 +43,7 @@ func (h *StudentHandler) GetStudent(w http.ResponseWriter, r *http.Request) {
 	}
 
 
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(student)
 }
 
@@ -75,4 +76,72 @@ func (h *StudentHandler) CreateStudent(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 
 	json.NewEncoder(w).Encode(student)
+}
+
+func (h *StudentHandler) UpdateStudent(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+
+	var student models.Student
+
+	err = json.NewDecoder(r.Body).Decode(&student)
+
+	if err != nil {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
+
+
+	success := h.Service.UpdateStudent(id, student)
+
+	if !success {
+		http.Error(w, "student not found", http.StatusNotFound)
+		return
+	}
+
+
+	h.Service.Save()
+
+
+	updatedStudent := h.Service.SearchStudent(id)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(updatedStudent)
+}
+
+func (h *StudentHandler) DeleteStudent(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+
+	success := h.Service.DeleteStudent(id)
+
+	if !success {
+		http.Error(w, "student not found", http.StatusNotFound)
+		return
+	}
+
+
+	h.Service.Save()
+
+
+	w.WriteHeader(http.StatusNoContent)
 }
